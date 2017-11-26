de13cc1b8641e54e3d28a76982a4ba62eed10190
Thomas_Pei <peit@brandeis.edu>
2017-11-26 01:50:47 -0500

    basic program

--- BlackJack.java
+++ BlackJack.java
@@ -3,15 +3,32 @@ import java.util.Scanner;
 public class BlackJack{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Welcome to BlackJack");
-		System.out.println("If you need any help, press '9' now.");
-		System.out.println("If not press 0.");
-		int help = scanner.nextInt(); scanner.nextLine();
-		if(help == 9){
-			blackjackManual();
-		}
-		System.out.println("Alright! Here are your first two cards.");
-		generateCard1();
-		
+		String[][] Cards = cardList(); //please make the method for the array of all cards
+		int end = 0;
+		do{
+			System.out.println("Welcome to BlackJack");
+			System.out.println("If you need any help, press '9' now.");
+			System.out.println("If not press 0.");
+			int help = scanner.nextInt(); scanner.nextLine();
+			if(help == 9){
+				blackjackManual(); //done
+			}
+			int computer = computerValue(); //generate random computer card values.
+			System.out.println("Alright! Here are your first two cards.");
+			String result = generateCard1(); //generate first cards in a method
+			System.out.printf("%s%n",result);
+			int sum = cardValue(result); //get the numeric value based on the card in a method
+			System.out.println("Do you want to hit, stand, or switch ace?");
+			String choice = scanner.nextLine();
+			sum = userOption(choice); //change the sum based on the use choice in a method
+			if(sum==21 || sum-computer>0){
+				System.out.println("Congrats! You win!");
+			}else{
+				System.out.println("The computer won with a hand value of %d",computer);
+			}
+			System.out.println("Do you want to try again?");
+			System.out.println("If so enter a number other than 0");
+			end = scanner.getInt(); scanner.getLine();
+		}while(end==0);
 	}
 }
