27342471341f4a864a6184cfeffc0a2294e10dde
Thomas_Pei <peit@brandeis.edu>
2017-11-27 19:07:58 -0500

    todai

--- generateCard1.java
+++ generateCard1.java
@@ -2,10 +2,10 @@
 public static String generateCard1(String[][] Cards){
 	int High = 6
 	int Low = 0
-	int first = random.nextInt(High-Low) + Low;
+	int first = random.nextInt(High);
 	int Hi = 7;
 	int Lo = 0;
-	int second = random.nextInt(Hi-Lo) + Lo;
+	int second = random.nextInt(Hi);
 	return Cards[first][second];
 	}
 }
